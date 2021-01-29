933779556e9b2e3c0666c76824c07a0dcd2d7d6e
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-01-29 13:52:43 -0800

    Add a 'Nothing' display if no toppings are selected'

--- js/scripts.js
+++ js/scripts.js
@@ -22,7 +22,6 @@ $(document).ready(function (){
   $("#pizzaForm").submit(function(event){
     event.preventDefault();
     let toppings = [];
-    let toppingsList = "";
     const size = $("input:radio[name=size]:checked").val();
     $("input:checkbox[name=toppings]:checked").each(function(){
       const item = $(this).val();
@@ -31,12 +30,15 @@ $(document).ready(function (){
     newPizza = new Pizza(size, toppings);
     let price = newPizza.pizzaPrice();
     $(".pizzaSize").text(size);
-    for( let i=0; i < toppings.length; i++) {
-      $(".pizzaToppings").append("<li>" + toppings[i]) + "</li>";
-    }
-    
+    if (toppings.length === 0) {
+      $(".pizzaToppings").append("<li>Nothing</li>");
+    } else {
+      for(let i=0; i < toppings.length; i++) {
+        $(".pizzaToppings").append("<li>" + toppings[i]) + "</li>";
+      }
+    };
     $(".pizzaCost").text((Math.round(price * 100) / 100).toFixed(2));
     $(".pizzaOrder").show();
     $("#placeOrder").hide();
-  })
-})
+  });
+});
